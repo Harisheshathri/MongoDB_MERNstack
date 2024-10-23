564cc6d8819701b6dc0dee87595013eb898ac276
Harisheshathri <146439895+Harisheshathri@users.noreply.github.com>
2024-10-23 18:24:15 +0530

    Add files via upload

---
+++ server/utils/dbConfig.js
@@ -0,0 +1,7 @@
+require('dotenv').config()
+
+const config = {
+    mongoDB: "mongodb+srv://sakthideepa77:dhaya.2003@hrent.vklg1.mongodb.net/?retryWrites=true&w=majority&appName=hrent"
+};
+
+module.exports = config;
